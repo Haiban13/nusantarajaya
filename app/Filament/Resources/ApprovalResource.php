79d11921be8673cb879542a9759b40937e6c3025
Haiban13 <haibanirwnv@gmail.com>
2025-08-29 19:32:22 +0700

    add user can create artikel with super admin approval(approval filament resource)

---
+++ app/Filament/Resources/ApprovalResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ApprovalResource\Pages;
+use App\Filament\Resources\ApprovalResource\RelationManagers;
+use App\Models\Approval;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ApprovalResource extends Resource
+{
+    protected static ?string $model = Approval::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('artikel_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Toggle::make('approve')
+                    ->required(),
+                Forms\Components\TextInput::make('approved_by')
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('artikel_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('approve')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('approved_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListApprovals::route('/'),
+            'create' => Pages\CreateApproval::route('/create'),
+            'edit' => Pages\EditApproval::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
             }
         });
     }
+
+    public function approvedArtikels()
+    {
+        return $this->hasMany(Approval::class, 'approved_by');
+    }
 }
